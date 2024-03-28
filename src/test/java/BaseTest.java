2dffa64fb05ec756c5047b4197fdf72096f94dd1
YanaN <yn55510@gmail.com>
2024-03-28 18:43:10 +0200

    change BaseTest for jenkins

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,42 +1,38 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class BaseTest {
     protected WebDriver webDriver;
 
     protected String url = "https://krauff.store";
 
+    @BeforeClass
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    }
+
     @BeforeMethod(alwaysRun = true)
     public void initDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--window-size=1980,1024");
+        options.addArguments("--headless");
 
         webDriver = new ChromeDriver(options);
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
     }
 
     @AfterMethod(alwaysRun = true)
     public void destroy() {
         webDriver.quit();
     }
-
-    // HELPER FUNCTIONS ------------------------------------
-
-
-//TODO: remove after usage
-    public List<WebElement> getElementsByXpath(String xpath) {
-        return webDriver.findElements(By.xpath(xpath));
-    }
-
 
 }
